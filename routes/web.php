0ee982367eef4ff6b88cada1162210302c285ccf
Jose Carlos Estrada <ingjocen@gmail.com>
2023-02-08 14:57:34 -0600

    Controladores y rutas

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 /**
  * Route::get       |   Consultar
@@ -21,18 +11,18 @@ use Illuminate\Support\Facades\Route;
  * Route::put       |   Actualizar
  */
 
-Route::get('/', function () {
-    return view('welcome');
+ /*
+Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('blog', [PageController::class, 'blog'])->name('blog');
+Route::get('blog/{slug}', [PageController::class, 'post'])->name('post');
+*/
+
+Route::controller(PageController::class)->group(function(){
+    Route::get('/' ,             'home' ) ->name('home');
+    Route::get('blog' ,          'blog' ) ->name('blog');
+    Route::get('blog/{slug}' ,   'post' ) ->name('post');
 });
 
-Route::get('blog', function () {
-    return 'Listado de publicaciones';
-});
-
-Route::get('blog/{slug}', function ($slug) {
-    return $slug;
-});
-
-Route::get('buscar', function (Request $request) {
-    return $request->all();
-});
+// Route::get('buscar', function (Request $request) {
+//     return $request->all();
+// });
